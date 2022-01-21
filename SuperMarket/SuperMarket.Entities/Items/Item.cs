71f4530f269ce3411b0a56d5c7721fa911a7c859
za3zilla <ahmed.selmi.esprit@gmail.com>
2022-01-21 23:43:00 +0100

    [Main] Add Item

---
+++ SuperMarket/SuperMarket.Entities/Items/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SuperMarket.Entities.Items
+{
+    public class Item
+    {
+        public Item(string name,decimal price)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ SuperMarket/SuperMarket.UnitTests/Items/ItemTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarket.Entities.Items;
+
+namespace SuperMarket.UnitTests.Items
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void ItemShouldHaveData()
+        {
+            string itemName = "can of beans";
+            decimal itemPrice = 0.65m; 
+            Item item = new Item(itemName, itemPrice);
+
+            //New item created
+            Assert.IsNotNull(item);
+
+            //Check that the item has an unique identifier
+            Assert.IsNotNull(item.Id);
+
+            //Check that the item has the right name
+            Assert.AreEqual(itemName, item.Name);
+
+            //Check that the item has the right price
+            Assert.AreEqual(itemPrice, item.Price);
+
+        }
+    }
+}
