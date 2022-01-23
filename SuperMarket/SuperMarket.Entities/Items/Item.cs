d686b8c34ebb499184db613b41bccda7fc144873
za3zilla <ahmed.selmi.esprit@gmail.com>
2022-01-23 09:40:26 +0100

    [Main] Add Rule To Item

--- SuperMarket/SuperMarket.Entities/Items/Item.cs
+++ SuperMarket/SuperMarket.Entities/Items/Item.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using SuperMarket.Entities.Rules;
+using System;
 
 namespace SuperMarket.Entities.Items
 {
     public class Item
     {
-        public Item(string name,decimal price)
+        public Item(string name, decimal price)
         {
             this.Id = Guid.NewGuid();
             this.Name = name;
@@ -14,5 +15,16 @@ namespace SuperMarket.Entities.Items
         public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public IRule Rule { get; private set; }
+
+        public void SetRule(IRule rule)
+        {
+            this.Rule = rule;
+        }
+
+        public decimal GetTotalPrice(int itemCount)
+        {
+            return this.Rule?.GetPrice(this.Price, itemCount) ?? itemCount * this.Price;
+        }
     }
 }
--- SuperMarket/SuperMarket.UnitTests/Items/ItemTests.cs
+++ SuperMarket/SuperMarket.UnitTests/Items/ItemTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SuperMarket.Entities.Items;
+using SuperMarket.Entities.Rules;
 
 namespace SuperMarket.UnitTests.Items
 {
@@ -25,6 +26,19 @@ namespace SuperMarket.UnitTests.Items
             //Check that the item has the right price
             Assert.AreEqual(itemPrice, item.Price);
 
+            //Check that the item has the right total price without rules
+            decimal expectedPrice = 2 * itemPrice;
+            Assert.AreEqual(expectedPrice, item.GetTotalPrice(itemCount: 2));
+
+            //Set a rule for the item
+            CountRule rule = new CountRule(name: "Three for a dollar", unit: 3, price: 1);
+            item.SetRule(rule);
+
+            //Check that the item has the right total price with rule
+            Assert.AreEqual(expectedPrice, item.GetTotalPrice(itemCount: 2));
+
+            expectedPrice = 1;
+            Assert.AreEqual(expectedPrice, item.GetTotalPrice(itemCount: 3));
         }
 
         [TestMethod]
